8e367a31a0c70bf308bc1610679f292a5b79aacf
IVAN BALABAY <i.balabai@gmail.com>
2022-08-11 21:26:01 +0300

    task18

---
+++ Seminar03/task18/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите Номер четверти:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+string GetCoord(int q)
+{
+   if (num == 1) return ("xc > 0 и yc > 0");
+   if (num == 2) return ("xc < 0 и yc > 0");
+   if (num == 3) return ("xc < 0 и yc < 0");
+   if (num == 4) return ("xc > 0 и yc < 0"); 
+   return "Введены некорректные координаты";
+}
+string result = GetCoord(num);
+Console.WriteLine(result);
