d09e5e0dfda1465bcdb60ae7d0a01c35633b9b09
IVAN BALABAY <i.balabai@gmail.com>
2022-08-11 15:23:22 +0300

    Task0 added

---
+++ Seminar02/Task09/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine ($"Случайное число из дипапзона 10 - 99 => {number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+string result = firstDigit == secondDigit ? "Цифры равны"
+: firstDigit > secondDigit ? firstDigit.ToString() : secondDigit.ToString();
+Console.WriteLine($"Наибольшая цифра числа {number} => {result}");
---
+++ Seminar02/task12/Program.cs
@@ -0,0 +1,3 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine ($"Случайное число из дипапзона 100 - 1000 => {number}");
+
