44513da2b9e99600e73eeb45d6630156c77768a5
IVAN BALABAY <i.balabai@gmail.com>
2022-08-11 16:05:48 +0300

    Task2 completed

---
+++ Seminar01/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A/B == B)
+    {
+        Console.WriteLine($"Число {A} является квадратом числа {B}");
+        }
+else
+{
+    Console.WriteLine($"Число {A}  не является квадратом числа {B}");
+}
